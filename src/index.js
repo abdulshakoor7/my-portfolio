2fff35af830b4d51113b191207dcc53f882880f9
Abdul Shakoor <134535584+abdulshakoor7@users.noreply.github.com>
2026-06-19 01:30:28 +0500

    Log confirmation of React app mounting
    Added a console log to confirm successful mounting of the React app.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// Ensure DOM is ready
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+// Log to confirm React mounted
+console.log('React app mounted successfully');
